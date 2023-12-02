d63c69a81c2ab0215dd14fa2ade456847d89031d
cnagar <cmdhakad93@gmail.com>
2023-12-02 22:36:30 +0530

    Create example01.cpp

---
+++ Threading/example01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+using namespace std;
+
+void threadA(){
+    for (int i = 0; i < 5; i++)
+    {
+        sleep(1);
+        cout<<"Thread A: "<<i<<endl;
+        fflush(stdout);
+    }
+    return ;
+    
+}
+
+void threadB(){
+    for (int i = 0; i < 5; i++)
+    {
+        sleep(1);
+        cout<<"Thread B: " << i<<endl;
+        fflush(stdout);
+    }
+    return;
+    
+}
+
+
+int main(){
+    thread t1(threadA);
+    thread t2(threadB);
+
+
+    return 0;
+}
